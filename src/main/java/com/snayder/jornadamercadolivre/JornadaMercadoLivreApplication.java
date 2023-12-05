ecbca0bcd9e1e33e14a00a213ec8aaa7ca841f0d
EsnayderGarcia <scriptk.18@gmail.com>
2023-12-05 20:30:41 -0300

    feat: configuração do projeto jornada-mercado-livre

---
+++ src/main/java/com/snayder/jornadamercadolivre/JornadaMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.snayder.jornadamercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaMercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaMercadoLivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snayder/jornadamercadolivre/JornadaMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snayder.jornadamercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
