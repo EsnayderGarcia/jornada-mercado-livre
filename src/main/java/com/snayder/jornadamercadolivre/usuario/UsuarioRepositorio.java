f97b756cdc33a66235130de1ba5f32b137e2bc9d
EsnayderGarcia <scriptk.18@gmail.com>
2023-12-12 23:45:14 -0300

    feat: método para buscar por email

--- src/main/java/com/snayder/jornadamercadolivre/usuario/UsuarioRepositorio.java
+++ src/main/java/com/snayder/jornadamercadolivre/usuario/UsuarioRepositorio.java
@@ -3,6 +3,9 @@ package com.snayder.jornadamercadolivre.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
